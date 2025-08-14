fd97650ac5a764922cf22988c3a801973ac15c33
leogogichaishvili <lgogi19@freeuni.edu.ge>
2025-08-14 21:52:14 +0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,307 @@
+/* eslint-disable */
+/** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin');
+
+const shorthands = plugin(function ({addUtilities}) {
+    addUtilities({
+        '.f-col': {
+            'display': 'flex',
+            'flex-direction': 'column',
+        },
+        '.f-row': {
+            'display': 'flex',
+            'flex-direction': 'row',
+        },
+        '.xy-center': {
+            'justify-content': 'center',
+            'align-items': 'center',
+        },
+        '.f-col-grow': {
+            'display': 'flex',
+            'flex-direction': 'column',
+            'flex-grow': 1,
+        },
+        '.f-grow': {
+            'display': 'flex',
+            'flex-direction': 'row',
+            'flex-grow': 1,
+        },
+        '.global-centered': {
+            position: 'fixed',
+            left: '50%',
+            transform: 'translateX(-50%)',
+            display: 'flex',
+            'flex-direction': 'row',
+            'flex-grow': 1,
+        }
+    })
+})
+
+module.exports = {
+    darkMode: ['class'],
+    content: [
+        './pages/**/*.{ts,tsx}',
+        './components/**/*.{ts,tsx}',
+        './app/**/*.{ts,tsx}',
+        './src/**/*.{ts,tsx}',
+        './src/**/*.{js,ts,jsx,tsx,html}',
+    ],
+    prefix: '',
+    theme: {
+        container: {
+            center: true,
+            screens: {
+                '2xl': '1400px'
+            }
+        },
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: 'var(--color-primary)',
+                    light: 'var(--color-primary-light)',
+                    dark: 'var(--color-aquatic-ultra-deep)',
+                },
+                secondary: {
+                    DEFAULT: 'var(--color-secondary)',
+                    light: 'var(--color-secondary-light)',
+                    dark: 'var(--color-secondary-dark)',
+                    foreground: 'var(--color-secondary-foreground)',
+                },
+                background: {
+                    light: 'var(--color-background-light)',
+                    dark: 'var(--color-blue-navy)',
+                },
+                border: {
+                    primary: {
+                        light: 'var(--color-card-light)',
+                        dark: 'var(--color-aquatic-main)',
+                        DEFAULT: 'var(--color-aquatic-main)',
+                    },
+                    secondary: {
+                        light: 'var(--color-grey-coal)',
+                        dark: 'var(--color-grey-coal)',
+                        DEFAULT: 'var(--color-grey-coal)',
+                    },
+                    hover: {
+                        light: 'var(--color-card-light)',
+                        dark: 'var(--color-ocean-depth)',
+                        DEFAULT: 'var(--color-ocean-depth)',
+                    },
+                    muted: {
+                        light: 'var(--color-card-light)',
+                        dark: 'var(--color-stroke-dark)',
+                        DEFAULT: 'var(--color-stroke-dark)',
+                    },
+                    accent: {
+                        light: 'var(--color-ocean-deep-shadow)',
+                        dark: 'var(--color-ocean-deep-shadow)',
+                        DEFAULT: 'var(--color-ocean-deep-shadow)',
+                    },
+                },
+                card: {
+                    primary: {
+                        light: 'var(--color-card-light)',
+                        dark: 'var(--color-ocean-depth)',
+                        DEFAULT: 'var(--color-ocean-depth)',
+                    },
+                    secondary: {
+                        light: 'var(--color-card-light)',
+                        dark: 'var(--color-ocean-depth)',
+                        DEFAULT: 'var(--color-ocean-depth)',
+                    },
+                    hover: {
+                        light: 'var(--color-card-light)',
+                        dark: 'var(--color-ocean-depth)',
+                        DEFAULT: 'var(--color-ocean-depth)',
+                    },
+                    muted: {
+                        light: 'var(--color-card-light)',
+                        dark: 'var(--color-ocean-depth)',
+                        DEFAULT: 'var(--color-ocean-depth)',
+                    },
+                    accent: {
+                        light: 'var(--color-ocean-deep-shadow)',
+                        dark: 'var(--color-ocean-deep-shadow)',
+                        DEFAULT: 'var(--color-ocean-deep-shadow)',
+                    },
+                },
+                island: {
+                    light: 'var(--color-island-light)',
+                    dark: 'var(--color-ocean-depth)',
+                },
+                icon: {
+                    DEFAULT: 'var(--color-icon-light)',
+                    light: 'var(--color-icon-light)',
+                    dark: 'var(--color-icon-dark)',
+                },
+                text: {
+                    primary: {
+                        light: 'var(--color-text-primary-light)',
+                        dark: 'var(--color-text-primary-dark)',
+                    },
+                    secondary: {
+                        light: 'var(--color-text-secondary-light)',
+                        dark: 'var(--color-text-secondary-dark)',
+                    },
+                },
+                stroke: {
+                    light: 'var(--color-stroke-light)',
+                    dark: 'var(--color-stroke-dark)',
+                },
+                error: {
+                    light: 'var(--color-error-light)',
+                    dark: 'var(--color-error-dark)',
+                },
+                warning: {
+                    light: 'var(--color-warning-light)',
+                    dark: 'var(--color-warning-dark)',
+                },
+                success: {
+                    light: 'var(--color-success-light)',
+                    dark: 'var(--color-success-dark)',
+                },
+                info: {
+                    light: 'var(--color-info-light)',
+                    dark: 'var(--color-info-dark)',
+                },
+                sidebar: {
+                    DEFAULT: 'var(--sidebar-background)',
+                    foreground: 'var(--sidebar-foreground)',
+                    primary: 'var(--sidebar-primary)',
+                    'primary-foreground': 'var(--sidebar-primary-foreground)',
+                    accent: 'var(--sidebar-accent)',
+                    'accent-foreground': 'var(--sidebar-accent-foreground)',
+                    border: 'var(--sidebar-border)',
+                    ring: 'var(--sidebar-ring)',
+                },
+                foreground: 'var(--foreground)',
+                popover: {
+                    DEFAULT: 'var(--popover)',
+                    foreground: 'var(--popover-foreground)',
+                },
+                muted: {
+                    DEFAULT: 'var(--muted)',
+                    foreground: 'var(--muted-foreground)',
+                },
+                accent: {
+                    DEFAULT: 'var(--accent)',
+                    foreground: 'var(--accent-foreground)',
+                },
+                destructive: {
+                    DEFAULT: 'var(--destructive)',
+                    foreground: 'var(--destructive-foreground)',
+                },
+                input: 'var(--color-primary-dark)',
+                ring: 'var(--color-primary-dark)',
+                chart: {
+                    '1': 'var(--chart-1)',
+                    '2': 'var(--chart-2)',
+                    '3': 'var(--chart-3)',
+                    '4': 'var(--chart-4)',
+                    '5': 'var(--chart-5)',
+                }
+            },
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)'
+            },
+            keyframes: {
+                'slide-in-from-right': {
+                    '0%': {transform: 'translateX(100%)'},
+                    '100%': {transform: 'translateX(0)'},
+                },
+                'caret-blink': {
+                    '0%,70%,100%': {
+                        opacity: '1'
+                    },
+                    '20%,50%': {
+                        opacity: '0'
+                    }
+                },
+                'accordion-down': {
+                    from: {
+                        height: '0'
+                    },
+                    to: {
+                        height: 'var(--radix-accordion-(content)-height)'
+                    }
+                },
+                'accordion-up': {
+                    from: {
+                        height: 'var(--radix-accordion-(content)-height)'
+                    },
+                    to: {
+                        height: '0'
+                    }
+                },
+                slideUp: {
+                    '0%': {transform: 'translateY(100%)', opacity: 0},
+                    '100%': {transform: 'translateY(0)', opacity: 1},
+                },
+                slideDown: {
+                    '0%': {transform: 'translateY(0)', opacity: 0},
+                    '100%': {transform: 'translateY(100%)', opacity: 1},
+                },
+                fadeOut: {
+                    '0%': {opacity: 1},
+                    '100%': {opacity: 0},
+                },
+                float: {
+                    '0%, 100%': {transform: 'translateY(0)'},
+                    '50%': {transform: 'translateY(-3px)'},
+                },
+                marquee: {
+                    '0%': {transform: 'translateX(0%)'},
+                    '100%': {transform: 'translateX(-50%)'},
+                },
+                shine: {
+                    "0%": {backgroundPosition: "100%"},
+                    "100%": {backgroundPosition: "-100%"},
+                },
+                'star-movement-bottom': {
+                    '0%': {transform: 'translate(0%, 0%)', opacity: '1'},
+                    '100%': {transform: 'translate(-100%, 0%)', opacity: '0'},
+                },
+                'star-movement-top': {
+                    '0%': {transform: 'translate(0%, 0%)', opacity: '1'},
+                    '100%': {transform: 'translate(100%, 0%)', opacity: '0'},
+                },
+            },
+            animation: {
+                'accordion-down': 'accordion-down 0.2s ease-out',
+                'accordion-up': 'accordion-up 0.2s ease-out',
+                'caret-blink': 'caret-blink 1.25s ease-out infinite',
+                'slide-up': 'slideUp 0.3s ease-in-out',
+                'slide-down': 'slideUp 0.3s ease-in-out',
+                'fade-out': 'fadeOut 0.3s ease-in-out',
+                'float': 'float 1s ease-linear infinite',
+                'marquee': 'marquee 20s linear infinite',
+                'shine': 'shine linear infinite',
+                'star-movement-bottom': 'star-movement-bottom linear infinite alternate',
+                'star-movement-top': 'star-movement-top linear infinite alternate',
+            },
+            fontFamily: {
+                poppins: [
+                    'Poppins',
+                    'sans-serif'
+                ],
+                roboto: ['var(--font-roboto)', 'sans-serif'],
+            },
+            fontSize: {
+                '4xl': '2.25rem', // 36px
+                '5xl': '3rem',    // 48px
+                '6xl': '6rem',    // 48px
+                '7xl': '6rem',    // 4
+                '8xl': '12rem',    //
+                '9xl': '16rem',
+                '10xl': '20rem',    // 48px
+            },
+        }
+    },
+    plugins: [
+        require('tailwindcss-animate'),
+        shorthands,
+    ],
+}
